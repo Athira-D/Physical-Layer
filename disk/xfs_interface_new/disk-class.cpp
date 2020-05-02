1be23daf676dd8b3c7f32e3524e537d95a5ee7fb
Athira Dileepkumar <athi6028@gmail.com>
2020-05-02 15:17:22 +0530

    Minor edits in disk class file

--- disk/xfs_interface_new/disk-class.cpp
+++ disk/xfs_interface_new/disk-class.cpp
@@ -6,14 +6,14 @@ public:
 		fseek(disk-copy, 0, SEEK_SET);
 		FILE *disk=fopen("disk","rb");
 		fseek(disk, 0, SEEK_SET);
-		unsigned char ch[2048];
+		unsigned char ch[BLOCK_SIZE];
 
 		for(int i=0;i<8192;i++)
 		{
 			fseek(disk,i*BLOCK_SIZE,SEEK_SET);
 			fseek(disk-copy,i*BLOCK_SIZE,SEEK_SET);
 			fread(ch,BLOCK_SIZE,1,disk);
-			fwrite(ch,2048,1,disk-copy);
+			fwrite(ch,BLOCK_SIZE,1,disk-copy);
 		}
 		fclose(disk);
 		fclose(disk-copy);
@@ -25,14 +25,14 @@ public:
 		fseek(disk-copy, 0, SEEK_SET);
 		FILE *disk=fopen("disk","wb+");
 		fseek(disk, 0, SEEK_SET);
-		unsigned char ch[2048];
+		unsigned char ch[BLOCK_SIZE];
 
 		for(int i=0;i<8192;i++)
 		{
 			fseek(disk,i*BLOCK_SIZE,SEEK_SET);
 			fseek(disk-copy,i*BLOCK_SIZE,SEEK_SET);
 			fread(ch,BLOCK_SIZE,1,disk-copy);
-			fwrite(ch,2048,1,disk);
+			fwrite(ch,BLOCK_SIZE,1,disk);
 		}
 		fclose(disk);
 		fclose(disk-copy);
@@ -42,18 +42,18 @@ public:
   	{
 		//args 1.disk block no, 2.memory buffer.
 		FILE *disk=fopen("disk_copy","rb");
-		int offset = block_no * BLOCK_SIZE;
+		int offset = blockNum * BLOCK_SIZE;
 		fseek(disk, offset, SEEK_SET);
-		fread(buffer, BLOCK_SIZE, 1, disk);
+		fread(block, BLOCK_SIZE, 1, disk);
 		fclose(disk);
 	}
 
 	static int writeBlock(unsigned char *block, int blockNum)
 	{
 		FILE *disk=fopen("disk_copy","wb");
-		int offset = block_no * BLOCK_SIZE;
+		int offset = blockNum * BLOCK_SIZE;
 		fseek(disk, offset, SEEK_SET);
-		fwrite(buffer, BLOCK_SIZE, 1, disk);
+		fwrite(block, BLOCK_SIZE, 1, disk);
 		fclose(disk);
 	}
 };
