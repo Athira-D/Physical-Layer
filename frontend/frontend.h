ed1666a0e83a46f9d7d2fbf00d244b7ea059cd27
pravitha-sathyavan <46380586+pravitha-sathyavan@users.noreply.github.com>
2020-06-02 12:56:04 +0700

    Add files via upload

--- frontend/frontend.h
+++ frontend/frontend.h
@@ -1,12 +1,26 @@
+
+#define INT 0
+#define STRING 1
+#define FLOAT 2
+#define EQ 101
+#define LE 102
+#define LT 103
+#define GE 104
+#define GT 105
+#define NE 106 //if considered
+#define RST 100 //reset op.
+#define PRJCT 107
+
+
 typedef struct fnode{
-	char str[ATTR_SIZE];
+	char str[16];
 	int ival;
 	struct fnode *right;
 	struct fnode *next;
 } fnode;
 
 typedef struct Condition{
-	char attr[ATTR_SIZE];
+	char attr[16];
 	int op;
-	char value[ATTR_SIZE];
+	char value[16];
 } Condition;
